331d6e0fa6e2967329573394715030205525e063
YaBoyPerun <124041530+YaBoyPerun@users.noreply.github.com>
2023-03-20 10:27:14 +0100

    Add files via upload

---
+++ EsDnevnikP2.0/EsDnevnik/Raspodela.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EsDnevnik
+{
+    partial class Raspodela
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Obrisi = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Menjaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Dodaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nastavnik = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Godina = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Predmet = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Odeljenje = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Obrisi,
+            this.Menjaj,
+            this.Dodaj,
+            this.id,
+            this.Nastavnik,
+            this.Godina,
+            this.Predmet,
+            this.Odeljenje});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 60);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(959, 398);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Obrisi
+            // 
+            this.Obrisi.HeaderText = "Obrisi";
+            this.Obrisi.MinimumWidth = 6;
+            this.Obrisi.Name = "Obrisi";
+            this.Obrisi.Text = "Obrisi";
+            this.Obrisi.UseColumnTextForButtonValue = true;
+            this.Obrisi.Width = 125;
+            // 
+            // Menjaj
+            // 
+            this.Menjaj.HeaderText = "Menjaj";
+            this.Menjaj.MinimumWidth = 6;
+            this.Menjaj.Name = "Menjaj";
+            this.Menjaj.Text = "Menjaj";
+            this.Menjaj.UseColumnTextForButtonValue = true;
+            this.Menjaj.Width = 125;
+            // 
+            // Dodaj
+            // 
+            this.Dodaj.HeaderText = "Dodaj";
+            this.Dodaj.MinimumWidth = 6;
+            this.Dodaj.Name = "Dodaj";
+            this.Dodaj.Text = "Dodaj";
+            this.Dodaj.UseColumnTextForButtonValue = true;
+            this.Dodaj.Width = 125;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 125;
+            // 
+            // Nastavnik
+            // 
+            this.Nastavnik.HeaderText = "Nastavnik";
+            this.Nastavnik.MinimumWidth = 6;
+            this.Nastavnik.Name = "Nastavnik";
+            this.Nastavnik.Width = 125;
+            // 
+            // Godina
+            // 
+            this.Godina.HeaderText = "Godina";
+            this.Godina.MinimumWidth = 6;
+            this.Godina.Name = "Godina";
+            this.Godina.Width = 125;
+            // 
+            // Predmet
+            // 
+            this.Predmet.HeaderText = "Predmet";
+            this.Predmet.MinimumWidth = 6;
+            this.Predmet.Name = "Predmet";
+            this.Predmet.Width = 125;
+            // 
+            // Odeljenje
+            // 
+            this.Odeljenje.HeaderText = "Odeljenje";
+            this.Odeljenje.MinimumWidth = 6;
+            this.Odeljenje.Name = "Odeljenje";
+            this.Odeljenje.Width = 125;
+            // 
+            // Raspodela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 484);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Raspodela";
+            this.Text = "Raspodela";
+            this.Load += new System.EventHandler(this.Raspodela_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Obrisi;
+        private System.Windows.Forms.DataGridViewButtonColumn Menjaj;
+        private System.Windows.Forms.DataGridViewButtonColumn Dodaj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Nastavnik;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Godina;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Predmet;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Odeljenje;
+    }
+}
---
+++ EsDnevnikP2.0/EsDnevnik/Upisnica.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace EsDnevnik
+{
+    partial class Upisnica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Obrisi = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Menjaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Dodaj = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ime_i_prezime = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Odeljenje = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Smer = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Obrisi,
+            this.Menjaj,
+            this.Dodaj,
+            this.Id,
+            this.Ime_i_prezime,
+            this.Odeljenje,
+            this.Smer});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 69);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(977, 379);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Obrisi
+            // 
+            this.Obrisi.HeaderText = "Obrisi";
+            this.Obrisi.MinimumWidth = 6;
+            this.Obrisi.Name = "Obrisi";
+            this.Obrisi.Text = "Obrisi";
+            this.Obrisi.UseColumnTextForButtonValue = true;
+            this.Obrisi.Width = 125;
+            // 
+            // Menjaj
+            // 
+            this.Menjaj.HeaderText = "Menjaj";
+            this.Menjaj.MinimumWidth = 6;
+            this.Menjaj.Name = "Menjaj";
+            this.Menjaj.Text = "Menjaj";
+            this.Menjaj.UseColumnTextForButtonValue = true;
+            this.Menjaj.Width = 125;
+            // 
+            // Dodaj
+            // 
+            this.Dodaj.HeaderText = "Dodaj";
+            this.Dodaj.MinimumWidth = 6;
+            this.Dodaj.Name = "Dodaj";
+            this.Dodaj.Text = "Dodaj";
+            this.Dodaj.UseColumnTextForButtonValue = true;
+            this.Dodaj.Width = 125;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 125;
+            // 
+            // Ime_i_prezime
+            // 
+            this.Ime_i_prezime.HeaderText = "Ime i prezime";
+            this.Ime_i_prezime.MinimumWidth = 6;
+            this.Ime_i_prezime.Name = "Ime_i_prezime";
+            this.Ime_i_prezime.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Ime_i_prezime.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Ime_i_prezime.Width = 125;
+            // 
+            // Odeljenje
+            // 
+            this.Odeljenje.HeaderText = "Odeljenje";
+            this.Odeljenje.MinimumWidth = 6;
+            this.Odeljenje.Name = "Odeljenje";
+            this.Odeljenje.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Odeljenje.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Odeljenje.Width = 125;
+            // 
+            // Smer
+            // 
+            this.Smer.HeaderText = "Smer";
+            this.Smer.MinimumWidth = 6;
+            this.Smer.Name = "Smer";
+            this.Smer.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Smer.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Smer.Width = 125;
+            // 
+            // Upisnica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 467);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Upisnica";
+            this.Text = "Upisnica";
+            this.Load += new System.EventHandler(this.Upisnica_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Obrisi;
+        private System.Windows.Forms.DataGridViewButtonColumn Menjaj;
+        private System.Windows.Forms.DataGridViewButtonColumn Dodaj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Ime_i_prezime;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Odeljenje;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Smer;
+    }
+}
